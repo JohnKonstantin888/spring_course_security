51ab0cb05f86e3844211dbee87f237098d839d74
Konstantin Kovalyov <Konstantin-kovalyo@yandex.ru>
2023-02-03 12:40:48 +0300

    spring security version 1

---
+++ src/main/java/com/konstantin/spring/security/configuration/MySecurityConfig.java
@@ -0,0 +1,35 @@
+package com.konstantin.spring.security.configuration;
+
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.User.UserBuilder;
+
+@EnableWebSecurity
+public class MySecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        UserBuilder userBuilder = User.withDefaultPasswordEncoder();
+        auth.inMemoryAuthentication()
+                .withUser(userBuilder.username("konstantin")
+                        .password("konstantin")
+                        .roles("EMPLOYEE"))
+                .withUser(userBuilder.username("sveta")
+                        .password("sveta")
+                        .roles("HR"))
+                .withUser(userBuilder.username("sergey")
+                        .password("sergey")
+                        .roles("MANAGER", "HR"));
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/").hasAnyRole("EMPLOYEE", "HR", "MANAGER")
+                .antMatchers("/hr_info").hasRole("HR")
+                .antMatchers("/manager_info/**").hasRole("MANAGER")
+                .and().formLogin().permitAll();
+    }
+}
---
+++ src/main/java/com/konstantin/spring/security/configuration/MySecurityInitializer.java
@@ -0,0 +1,6 @@
+package com.konstantin.spring.security.configuration;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class MySecurityInitializer extends AbstractSecurityWebApplicationInitializer {
+}
