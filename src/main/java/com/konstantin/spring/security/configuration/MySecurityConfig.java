08b652bc36a0f0ceb1a52e57e44ab66afb0a5462
Konstantin Kovalyov <Konstantin-kovalyo@yandex.ru>
2023-02-03 13:17:27 +0300

    spring security version 2 added DB & bcrypt

--- src/main/java/com/konstantin/spring/security/configuration/MySecurityConfig.java
+++ src/main/java/com/konstantin/spring/security/configuration/MySecurityConfig.java
@@ -1,5 +1,6 @@
 package com.konstantin.spring.security.configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -7,21 +8,16 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.User.UserBuilder;
 
+import javax.sql.DataSource;
+
 @EnableWebSecurity
 public class MySecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    DataSource dataSource;
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        UserBuilder userBuilder = User.withDefaultPasswordEncoder();
-        auth.inMemoryAuthentication()
-                .withUser(userBuilder.username("konstantin")
-                        .password("konstantin")
-                        .roles("EMPLOYEE"))
-                .withUser(userBuilder.username("sveta")
-                        .password("sveta")
-                        .roles("HR"))
-                .withUser(userBuilder.username("sergey")
-                        .password("sergey")
-                        .roles("MANAGER", "HR"));
+        auth.jdbcAuthentication().dataSource(dataSource);
     }
 
     @Override
